ee989951122de5f67d671ffcd877b027fface939
gilymoon <60445037+gilymoon@users.noreply.github.com>
2020-04-26 11:53:07 +0300

    Delete todolist.js

--- lesson5/task3/temp/scripts/todolist.js
+++
@@ -1,8 +0,0 @@
-import { onCreateTask } from "./taskCreator.js";
-import { onListClick } from "./taskChanger.js";
-export const initTodoListHandlers = () => {
-  const createBtnElem = document.querySelector(".create-task-btn");
-  createBtnElem.addEventListener("click", onCreateTask);
-  const todoListElem = document.querySelector(".list");
-  todoListElem.addEventListener("click", onListClick);
-};
